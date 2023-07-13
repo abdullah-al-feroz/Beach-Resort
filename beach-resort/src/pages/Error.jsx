7496a9360171cfb8881f6bdc1e21fbbb94b074ff
Md. Abdullah- Al Firoz <firozcse2020@gmail.com>
2023-07-14 02:23:33 +0600

    added multiple pages and routing implement successfully

---
+++ beach-resort/src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>Hello From Error Page</div>
+  )
+}
+
+export default Error
---
+++ beach-resort/src/pages/Room.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Room = () => {
+  return (
+    <div>Hello From Room</div>
+  )
+}
+
+export default Room
